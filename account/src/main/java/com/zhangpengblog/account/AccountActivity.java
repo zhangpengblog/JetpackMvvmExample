1b581d2f4db75ddf1f491fbb58693557729b49bd
zhangpengblog <775326029@qq.com>
2020-12-07 19:45:51 +0800

    app package account module mybase library

--- account/src/main/java/com/zhangpengblog/account/AccountActivity.java
+++ account/src/main/java/com/zhangpengblog/account/AccountActivity.java
@@ -1,14 +1,13 @@
 package com.zhangpengblog.account;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class AccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate (savedInstanceState);
-        setContentView (R.layout.activity_account);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
     }
 }
